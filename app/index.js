eda3051fabbd77833513d81f6db032948f6ba5dc
Adrian Andrzejewski <adix.adrian@gmail.com>
2015-07-24 15:33:03 +0200

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,170 @@
+'use strict';
+
+var generators = require('yeoman-generator');
+var yosay = require('yosay');
+var chalk = require('chalk');
+var wiredep = require('wiredep');
+var _s = require('underscore.string');
+var mkdirp = require('mkdirp');
+
+module.exports = generators.Base.extend({
+  constructor: function () {
+    generators.Base.apply(this, arguments);
+
+    this.option('skip-welcome-message', {
+      desc: 'Skips the welcome message',
+      type: Boolean
+    });
+  },
+
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    if (!this.options['skip-welcome-message']) {
+      this.log(yosay(
+        chalk.red('Welcome!') + '\n' +
+        chalk.yellow('You\'re using the Noke - generator for an sass application with gulp support!')
+        ));
+    }
+
+    var prompts = [{
+      name: 'nodeSass',
+      type: 'list',
+      message: 'Which CSS preprocessor do you want?',
+      default: true,
+      choices: [{
+        name: 'Node sass, use me, I\'m so fast!',
+        value: true
+      }, {
+        name: 'Ruby Sass, maybe I\'m slow but at least I don\'t need C++ to work..',
+        value: false
+      }]
+    },
+    {
+      name: "sprite",
+      type: 'list',
+      default: true,
+      message: 'Would you need automatic sprite generation?',
+      choices: [{
+        name: 'gulp.spritesmith please!',
+        value: true
+      }, {
+        name: 'Nope, I\'ll do sprites on my own',
+        value: false
+      }]
+    }];
+
+    this.prompt(prompts, function (answers) {
+      this.includeNodeSass = answers.nodeSass;
+      this.includeSprite = answers.sprite;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    gulpfile: function () {
+      this.fs.copyTpl(
+        this.templatePath('gulpfile.js'),
+        this.destinationPath('gulpfile.js'),
+        {
+          includeNodeSass: this.includeNodeSass,
+          includeSprite: this.includeSprite
+        }
+      );
+    },
+
+    packageJSON: function () {
+      this.fs.copyTpl(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json'),
+        {
+          includeNodeSass: this.includeNodeSass,
+          includeSprite: this.includeSprite
+        }
+      );
+    },
+
+    git: function () {
+      this.fs.copy(
+        this.templatePath('gitignore'),
+        this.destinationPath('.gitignore'));
+
+      this.fs.copy(
+        this.templatePath('gitattributes'),
+        this.destinationPath('.gitattributes'));
+    },
+
+    bower: function () {
+      var bowerJson = {
+        name: _s.slugify(this.appname),
+        private: true,
+        dependencies: {
+          "kiki": "latest"
+        }
+      };
+
+      this.fs.writeJSON('bower.json', bowerJson);
+      this.fs.copy(
+        this.templatePath('bowerrc'),
+        this.destinationPath('.bowerrc')
+      );
+    },
+
+    editorConfig: function () {
+      this.fs.copy(
+        this.templatePath('editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+    },
+
+    styles: function () {
+      mkdirp('css');
+      this.fs.copy(
+        this.templatePath('scss'),
+        this.destinationPath('scss')
+      );
+    },
+
+    images: function () {
+      mkdirp('images');
+      if (this.includeSprite) {
+        mkdirp('images/sprite');
+      }
+    }
+  },
+
+  install: function () {
+    this.installDependencies({
+      skipInstall: this.options['skip-install']
+    });
+  },
+
+  end: function () {
+    var bowerJson = this.fs.readJSON(this.destinationPath('bower.json'));
+    var howToInstall =
+      '\nAfter running ' +
+      chalk.yellow.bold('npm install & bower install') +
+      ', inject your' +
+      '\nfront end dependencies by running ' +
+      chalk.yellow.bold('gulp wiredep') +
+      '.';
+
+    if (this.options['skip-install']) {
+      this.log(howToInstall);
+      return;
+    }
+
+    wiredep({
+      bowerJson: bowerJson,
+      directory: 'bower_components',
+      ignorePath: /^(\.\.\/)+/,
+      src: 'scss/main.scss'
+    });
+  }
+
+});
---
+++ app/templates/gulpfile.js
@@ -0,0 +1,54 @@
+(function () {
+  'use strict';
+
+/*------------------------------------*\
+ #CSS STYLES TASKS
+\*------------------------------------*/
+  var gulp = require('gulp'),
+      autoprefixer = require('autoprefixer-core'),
+      mqpacker = require('css-mqpacker'),
+      opacity = require('postcss-opacity'),
+      pixrem = require('pixrem'),
+      $ = require('gulp-load-plugins')();
+
+  gulp.task('styles', function () {
+    var processors = [
+      autoprefixer({browsers: ['last 1 version']}),
+      mqpacker,
+      opacity,
+      pixrem
+    ];
+
+    return <% if (includeNodeSass) { %>gulp.src('scss/')
+      .pipe($.sass({errLogToConsole: true})) <% } else { %>$.rubySass('scss/main.scss') <% } %>
+      .pipe($.postcss(processors))
+      .pipe($.minifyCss())
+      .pipe($.rename({suffix: '.min'}))
+      .pipe($.size({showFiles: true}))
+      .on('error', console.error.bind(console))
+      .pipe(gulp.dest('css'));
+  }); 
+  <% if (includeSprite) { %>
+  gulp.task('sprite', function () {
+    var spriteData = gulp.src('images/sprite/**/*')
+      .pipe($.plumber())
+      .pipe($.spritesmith({
+        imgName: 'sprite.png',
+        imgPath: '../images/sprite.png',
+        cssName: '_sprite.scss',
+        padding: 5
+      }));
+    spriteData.img.pipe(gulp.dest('images/'));
+    return spriteData.css.pipe(gulp.dest('scss/components/'));
+  });<% } %>
+
+  gulp.task('dev', function () {
+    gulp.watch(['scss/**/*.{css,scss}'], ['styles']); <% if (includeSprite) { %> 
+    gulp.watch(['images/sprite/**/*'], ['sprite']); <% } %>
+  });
+/*------------------------------------*\
+ #END OF CSS STYLES TASKS
+\*------------------------------------*/
+
+})();
+
