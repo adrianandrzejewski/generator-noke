079907265e483358c39a68d18f4594e055fa41ca
Adrian Andrzejewski <adix.adrian@gmail.com>
2015-07-27 15:23:00 +0200

    Revert "Revert "Bumped version number""
    This reverts commit d44126cbb3c8017ea69d1e1d9b877a922f3f3615.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,25 +1,17 @@
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 
-
-const $ = gulpLoadPlugins(),
-    bump = require('gulp-bump'),
-    gutil = require('gulp-util'),
-    git = require('gulp-git'),
-    conventionalChangelog = require('gulp-conventional-changelog');
-
+const $ = gulpLoadPlugins();
 
 gulp.task('bump-version', () => {
   return gulp.src(['./package.json'])
-    .pipe(bump({type: 'patch'}).on('error', gutil.log))
+    .pipe($.bump({type: 'patch'}).on('error', $.util.log))
     .pipe(gulp.dest('./'));
 });
 
 gulp.task('generate-changelog', () => {
-  return gulp.src('CHANGELOG.md', {
-    buffer: false
-  })
-  .pipe(conventionalChangelog({
+  return gulp.src('CHANGELOG.md')
+  .pipe($.conventionalChangelog({
       preset: 'angular'
     }))
   .pipe(gulp.dest('./'));
@@ -27,20 +19,20 @@ gulp.task('generate-changelog', () => {
 
 gulp.task('commit-changes', () => {
   return gulp.src('.')
-    .pipe(git.commit('Bumped version number', {args: '-a'}));
+    .pipe($.git.commit('Bumped version number'));
 });
 
 gulp.task('push-changes', (cb) => {
-  git.push('origin', 'master', cb);
+  $.git.push('origin', 'master', cb);
 });
 
 gulp.task('create-new-tag', (cb) => {
   var version = getPackageJsonVersion();
-  git.tag(version, 'Created Tag for version: ' + version, function (error) {
+  $.git.tag(version, 'Created Tag for version: ' + version, function (error) {
     if (error) {
       return cb(error);
     }
-    git.push('origin', 'master', {args: '--tags'}, cb);
+    $.git.push('origin', 'master', {args: '--tags'}, cb);
   });
   function getPackageJsonVersion () {
     return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
